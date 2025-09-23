00fdcf49da5a798ae9f389d681840245b6f4d4a9
chatJohn <1059257007@qq.com>
2025-09-23 13:58:19 +0800

    fix emedding bugs

--- embedding/embedding.cu
+++ embedding/embedding.cu
@@ -74,27 +74,29 @@ __global__ void embedding_f16x8_pack_kernel(const int *idx, half *weight, half *
 
 #define STRINGFY(str) #str
 #define TORCH_BINDING_COMMON_EXTENSION(func) m.def(STRINGFY(func), &func, STRINGFY(func));
-#define CHECK_TORCH_TENSOR_DTYPE(T, th_type)
-    if (T.options().dtype() != th_type){
-        std::cout << "Tensor Info:" << T.options() << std::endl;
-        throw std::runtime_error("value must be " #th_type);
+#define CHECK_TORCH_TENSOR_DTYPE(T, th_type)                                                                \
+    if (T.options().dtype() != th_type){                                                                    \
+        std::cout << "Tensor Info:" << T.options() << std::endl;                                            \
+        throw std::runtime_error("value must be " #th_type);                                                \
     }
-#define CHECK_TORCH_TENSOR_SHAPE(T, S0, S1)
-    if (T.size(0) != S0 || T.size(1) != S1){
-        throw std::runtime_error("Tensor size mismatch!");    
+#define CHECK_TORCH_TENSOR_SHAPE(T, S0, S1)                                                                 \
+    if (T.size(0) != S0 || T.size(1) != S1){                                                                \
+        throw std::runtime_error("Tensor size mismatch!");                                                  \
     }
-#define TORCH_BINDING_EMBEDDING(packed_type, th_type, element_type, n_elements)
-    void embedding_##packed_type(torch::Tensor a, torch::Tensor weight, torch::Tensor o){
-        CHECK_TORCH_TENSOR_DTYPE(a, torch::kInt32);
-        CHECK_TORCH_TENSOR_DTYPE(weight, th_type);
-        CHECK_TORCH_TENSOR_DTYPE(o, th_type);
-        const int N = a.size(0);
-        const int emb_size = weight.size(1);
-        dim3 block(emb_size / n_elements);
-        dim3 grid(N); // each block handles one embedding
-        embedding_##packed_type##_kernel<<<grid, block>>>(reinterpret_cast<int *>(a.data_ptr())
-                                        , reinterpret_cast<element_type *>(weight.data_ptr())
-                                        , reinterpret_cast<element_type *>(o.data_ptr()), N, emb_size);   
+
+#define TORCH_BINDING_EMBEDDING(packed_type, th_type, element_type, n_elements)                             \
+    void embedding_##packed_type(torch::Tensor a, torch::Tensor weight, torch::Tensor o){                   \
+        CHECK_TORCH_TENSOR_DTYPE(a, torch::kInt32);                                                         \
+        CHECK_TORCH_TENSOR_DTYPE(weight, th_type);                                                          \
+        CHECK_TORCH_TENSOR_DTYPE(o, th_type);                                                               \
+        const int N = a.size(0);                                                                            \
+        const int emb_size = weight.size(1);                                                                \      
+        dim3 block(emb_size / n_elements);                                                                  \
+        /*one blocks handles one embedding vector*/                                                         \
+        dim3 grid(N);                                                                                       \   
+        embedding_##packed_type##_kernel<<<grid, block>>>(reinterpret_cast<int *>(a.data_ptr())             \
+                                        , reinterpret_cast<element_type *>(weight.data_ptr())               \
+                                        , reinterpret_cast<element_type *>(o.data_ptr()), N, emb_size);     \   
     }
 
 TORCH_BINDING_EMBEDDING(f32, torch::kFloat32, float, 1)
--- embedding/embedding.py
+++ embedding/embedding.py
@@ -49,7 +49,7 @@ def run_benchmark(
             perf_func(a, b, out)
     else:
         for i in range(iters):
-            _ = perf_func(a, b)
+            out = perf_func(a, b)
     torch.cuda.synchronize()
     end = time.time()
     total_time = (end - start) * 1000 # ms
@@ -63,3 +63,27 @@ def run_benchmark(
         print(out)
     return out.clone(), mean_time
 
+Ms = [1024, 4096] # max value of token_ids
+Ns = [2048, 4096]  # seqlen
+Ks = [512, 1024]  # embedding size
+MNKs = [(M, N, K) for M in Ms for N in Ns for K in Ks]
+for M, N, K in MNKs:
+    print("-" * 110)
+    print(" " * 45 + f"MaxV={M}, SeqLen={N}, EmbSize={K}")
+    i = torch.randint(0, M, size=(N,)).cuda().int().contiguous()
+    weight = torch.randn((M, K)).float().cuda().contiguous()
+    o = torch.zeros((N, K)).float().cuda().contiguous()
+
+    run_benchmark(lib.embedding_f32, i, weight, "f32", o)
+    run_benchmark(lib.embedding_f32x4, i, weight, "f32x4", o)
+    run_benchmark(lib.embedding_f32x4_pack, i, weight, "f32x4_pack", o)
+    run_benchmark(partial(embedding), i, weight, "f32_th")
+
+    print("-" * 110)
+    weight_f16 = torch.randn((M, K)).half().cuda().contiguous()
+    o_f16 = torch.zeros((N, K)).half().cuda().contiguous()
+    run_benchmark(lib.embedding_f16, i, weight_f16, "f16", o_f16)
+    run_benchmark(lib.embedding_f16x8, i, weight_f16, "f16x8", o_f16)
+    run_benchmark(lib.embedding_f16x8_pack, i, weight_f16, "f16x8_pack", o_f16)
+    run_benchmark(partial(embedding), i, weight_f16, "f16_th")
+    print("-" * 110)

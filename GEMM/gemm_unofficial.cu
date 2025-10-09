c6b2acf40cc742e037763960d6b2a047f54d8a73
chatJohn <1059257007@qq.com>
2025-10-09 23:45:21 +0800

    finish threadTile and wrapGEMM

--- GEMM/gemm_unofficial.cu
+++ GEMM/gemm_unofficial.cu
@@ -12,8 +12,7 @@ __global__ void naive_gemm(float *A, float *B, float *C, const int M, const int 
 
 
 // one block for one tile of C
-template<int Bm = 128, int Bn = 128, int Bk = 8, int blockSize = 256, int A_BLOCK_X = 8,
-        int B_BLOCK_X = 32, int C_BLOCK_X = 16>
+template<int Bm = 128, int Bn = 128, int Bk = 8, int blockSize = 256, int A_BLOCK_X = 8, int B_BLOCK_X = 32, int C_BLOCK_X = 16>
 __global__ void block_tile_GEMM(float *A, float *B, float *C, const int M, const int N, const int K){
     __shared__ float As[Bm][Bk];
     __shared__ float Bs[Bk][Bn];
@@ -95,4 +94,210 @@ __global__ void block_tile_GEMM(float *A, float *B, float *C, const int M, const
             }
     }
   }
+
+
+}
+
+// thread tile is based on the block tile, optiming the thread computation in the operation of sub_A and sub_B multiplication
+template<int Bm = 128, int Bn = 128, int Bk = 8, int blockSize = 256, int A_BLOCK_X = 8, int B_BLOCK_X = 32, int C_BLOCK_X = 16>
+__global__ void thhread_tile_GEMM(float *A, float *B, float *C, const int M, const int N, const int K){
+    __shared__ float As[Bm][Bk];
+    __shared__ float Bs[Bk][Bn];
+
+    int r0 = blockIdx.y * Bm;
+    int c0 = blockIdx.x * Bn;
+    int tid = threadIdx.x;
+
+    constexpr int A_BLOCK_Y = blockSize / A_BLOCK_X;
+    int A_THREAD_Y = tid / A_BLOCK_X;
+    int A_THREAD_X = tid % A_BLOCK_X;
+
+    constexpr int B_BLOCK_Y = blockSize / B_BLOCK_X;
+    int B_THREAD_Y = tid / B_BLOCK_X;
+    int B_THREAD_X = tid % B_BLOCK_X;
+
+    constexpr int C_BLOCK_Y = blockSize / C_BLOCK_X;
+    int C_THREAD_Y = tid / C_BLOCK_X;
+    int C_THREAD_X = tid % C_BLOCK_X;
+
+    constexpr int Tm = Bm / C_BLOCK_Y;
+    constexpr int Tn = Bn / C_BLOCK_X;
+
+    float Ct[Tm][Tn] = {0.f};
+
+    // thread tile
+    float regA[Tm] = {0.f};
+    float regB[Tn] = {0.f};
+
+    for(int k = 0; k < K; k += Bk){
+        #pragma unroll
+        for(int i = A_THREAD_Y; i < Bm; i += A_BLOCK_Y){
+            int r = r0 + i;
+            #pragma unroll
+            for(int j = A_THREAD_X; j < Bk; j += A_BLOCK_X){
+                int c = k + j;
+                As[i][j] = (r < M && c < K) ? A[r * K + c] : 0.f;
+            }
+        }
+
+        #pragma unroll
+        for(int i = B_THREAD_Y; i < Bk; i += B_BLOCK_Y){
+            int r = k + i;
+            #pragma unroll
+            for(int j = B_THREAD_X; j < Bn; j += B_BLOCK_X){
+                int c = c0 + j;
+                Bs[i][j] = (r < K && c < N) ? B[r * N + c] : 0.f;
+            }
+        }
+        __syncthreads();
+
+        #pragma unroll
+        for(int p = 0; p < Bk; p++){
+            // put element in `As` into regA
+            #pragma unroll
+            for(int i = 0; i < Tm; i++){
+                int r = C_THREAD_Y + i * C_BLOCK_Y;
+                regA[i] = As[r][p];
+            }
+            // same for Bs
+            #pragma unroll
+            for(int i = 0; i < Tn; i++){
+                int c = C_THREAD_X + i * C_BLOCK_X;
+                regB[i] = Bs[p][c];
+            }
+
+            // 矩阵外积
+            #pragma unroll
+            for(int i = 0; i < Tm; i++){
+                #pragma unroll
+                for(int j = 0; j < Tn; j++){
+                    Ct[i][j] += regA[i] * regB[j];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    #pragma unroll
+    for(int i = 0; i < Tm; i++){
+        int r = r0 + C_THREAD_Y + i * C_BLOCK_Y;
+        #pragma unroll
+        for(int j = 0; j <Tn; j++){
+            int c = c0 + C_THREAD_X + j * C_BLOCK_X;
+            if(r < M && c < N){
+                C[r * N + c] = Ct[i][j];
+            }
+        }
+    }
+
+
+}
+
+template<int Bm = 128, int Bn = 128, int Bk = 8, int blockSize = 256, int A_BLOCK_X = 8, int B_BLOCK_X = 32, int C_BLOCK_X = 16, int C_WARP_X = 8, int C_WARP_Y = 4>
+__global__ void wrap_GEMM(float *A, float *B, float *C, const int M, const int N, const int K){
+    __shared__ float As[Bm][Bk];
+    __shared__ float Bs[Bk][Bn];
+
+    int r0 = blockIdx.y * Bm;
+    int c0 = blockIdx.x * Bn;
+    int tid = threadIdx.x;
+
+    constexpr int A_BLOCK_Y = blockSize / A_BLOCK_X;
+    int A_THREAD_Y = tid / A_BLOCK_X;
+    int A_THREAD_X = tid % A_BLOCK_X;
+
+    constexpr int B_BLOCK_Y = blockSize / B_BLOCK_X;
+    int B_THREAD_Y = tid / B_BLOCK_X;
+    int B_THREAD_X = tid % B_BLOCK_X;
+
+    // fix wrap size, which is different from `block_tile_GEMM` and `thread_tile_GEMM`
+    // change below
+    constexpr int C_BLOCK_Y = blockSize / C_BLOCK_X;
+    constexpr int WRAP_SIZE = C_WARP_X * C_WARP_Y;
+    int wrapId = tid / WRAP_SIZE;
+    int laneId = tid % WRAP_SIZE;
+
+    constexpr int C_WRAP_DIM_X = C_BLOCK_X / C_WARP_X;
+    int wrapX = wrapId % C_WRAP_DIM_X;
+    int wrapY = wrapId / C_WRAP_DIM_X;
+    int laneY = laneId / C_WARP_X;
+    int laneX = laneId % C_WARP_X;
+    int C_THREAD_Y = wrapY * C_WARP_Y + laneY;
+    int C_THREAD_X = wrapX * C_WARP_X + laneX;
+
+
+
+
+
+    // change up
+    constexpr int Tm = Bm / C_BLOCK_Y;
+    constexpr int Tn = Bn / C_BLOCK_X;
+
+    float Ct[Tm][Tn] = {0.f};
+
+    // thread tile
+    float regA[Tm] = {0.f};
+    float regB[Tn] = {0.f};
+
+    for(int k = 0; k < K; k += Bk){
+        #pragma unroll
+        for(int i = A_THREAD_Y; i < Bm; i += A_BLOCK_Y){
+            int r = r0 + i;
+            #pragma unroll
+            for(int j = A_THREAD_X; j < Bk; j += A_BLOCK_X){
+                int c = k + j;
+                As[i][j] = (r < M && c < K) ? A[r * K + c] : 0.f;
+            }
+        }
+
+        #pragma unroll
+        for(int i = B_THREAD_Y; i < Bk; i += B_BLOCK_Y){
+            int r = k + i;
+            #pragma unroll
+            for(int j = B_THREAD_X; j < Bn; j += B_BLOCK_X){
+                int c = c0 + j;
+                Bs[i][j] = (r < K && c < N) ? B[r * N + c] : 0.f;
+            }
+        }
+        __syncthreads();
+
+        #pragma unroll
+        for(int p = 0; p < Bk; p++){
+            // put element in `As` into regA
+            #pragma unroll
+            for(int i = 0; i < Tm; i++){
+                int r = C_THREAD_Y + i * C_BLOCK_Y;
+                regA[i] = As[r][p];
+            }
+            // same for Bs
+            #pragma unroll
+            for(int i = 0; i < Tn; i++){
+                int c = C_THREAD_X + i * C_BLOCK_X;
+                regB[i] = Bs[p][c];
+            }
+
+            // 矩阵外积
+            #pragma unroll
+            for(int i = 0; i < Tm; i++){
+                #pragma unroll
+                for(int j = 0; j < Tn; j++){
+                    Ct[i][j] += regA[i] * regB[j];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    #pragma unroll
+    for(int i = 0; i < Tm; i++){
+        int r = r0 + C_THREAD_Y + i * C_BLOCK_Y;
+        #pragma unroll
+        for(int j = 0; j <Tn; j++){
+            int c = c0 + C_THREAD_X + j * C_BLOCK_X;
+            if(r < M && c < N){
+                C[r * N + c] = Ct[i][j];
+            }
+        }
+    }
+
 }
